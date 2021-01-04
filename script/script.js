4edefde7986f909c1830bb370a4607f23bf89fef
Snigdho Dip Howlader <61914319+snigdho611@users.noreply.github.com>
2021-01-04 08:43:31 +0600

    update

--- script/script.js
+++ script/script.js
@@ -1,9 +1,10 @@
 document.getElementById('memberPhoto').hidden = true;
-let x=3;
+let x=7;
 
 function go()
 {
-    //x = Math.random(1,7);
+    console.log(x);
+    x = Math.floor(Math.random() * (7 - 1 + 1)) + 1;;
     document.getElementById('memberPhoto').hidden = false;
     if(x == 1)
     {
@@ -25,5 +26,33 @@ function go()
         document.getElementById('memberPhoto').width = '280';
         document.getElementById('memberPhoto').height = '300';
         document.getElementById('memberName').textContent = 'Kim Namjoon';
+    }
+    else if(x == 4)
+    {
+        document.getElementById('memberPhoto').src = './images/kimseokjin.jpg';
+        document.getElementById('memberPhoto').width = '250';
+        document.getElementById('memberPhoto').height = '300';
+        document.getElementById('memberName').textContent = 'Kim Seokjin';
+    }
+    else if(x == 5)
+    {
+        document.getElementById('memberPhoto').src = './images/kimtaehyung.jpg';
+        document.getElementById('memberPhoto').width = '250';
+        document.getElementById('memberPhoto').height = '300';
+        document.getElementById('memberName').textContent = 'Kim Taehyung/V';
+    }
+    else if(x == 6)
+    {
+        document.getElementById('memberPhoto').src = './images/minyoongi.jpg';
+        document.getElementById('memberPhoto').width = '310';
+        document.getElementById('memberPhoto').height = '300';
+        document.getElementById('memberName').textContent = 'Min Yoongi/Suga';
+    }
+    else if(x == 7)
+    {
+        document.getElementById('memberPhoto').src = './images/parkjimin.jpg';
+        document.getElementById('memberPhoto').width = '250';
+        document.getElementById('memberPhoto').height = '320';
+        document.getElementById('memberName').textContent = 'Park Jimin';
     }
 }
